1e5c7f4b642a8542de971bdbb516646b27c0593c
Nedas Necepurenko <nedas@192.168.1.107>
2023-10-05 16:08:53 +0100

    Added leaky relu and ELU activation functions

--- activation_functions.py
+++ activation_functions.py
@@ -1,4 +1,6 @@
 import math
+import numpy as n
+import pandas as p
 
 def activation_logistic(input):
     return (1 + (math.e ** -input)) ** -1
@@ -8,3 +10,12 @@ def activation_relu(input):
 
 def activation_tanh(input):
     return math.tanh(input)
+
+def activation_leaky_relu(input):
+    return max(0.03*input,input)
+
+def activation_elu(input):
+    if input > 0:
+        return input
+    else:
+        return (n.exp(input)-1)
