520c8ad4e2fb4c59d0a245281f3d3c294795ac5a
Tikhon McGill <tm2008@hw.ac.uk>
2023-10-16 11:45:41 +0100

    Create profile creation method, add 4 new profiles

--- hyperparameter_profile.py
+++ hyperparameter_profile.py
@@ -1,8 +1,30 @@
 import activation_functions as actfunc
 
-#profile class to easily adjust the number of hidden layers with their respective number of neurons and the actiavtion function
-def profile1():
-    hidden_layers = [4,4]
-    activation_function = actfunc.activation_relu
+# Profile class to easily adjust the number of hidden layers
+# with their respective number of neurons and the activation function
+
+# Generic function for creating a profile
+def profile(_hidden_layers,_activation_function):
+    hidden_layers = _hidden_layers
+    activation_function = _activation_function
     return hidden_layers, activation_function
 
+#Create profile with 2 hidden layers of 4 neurons each, utilizing the ReLU activation function
+def profile1():
+    return profile([4,4],actfunc.activation_relu)
+
+#Create profile with 2 hidden layers of 4 neurons each, utilizing the logistic activation function
+def profile2():
+    return profile([4,4],actfunc.activation_logistic)
+
+#Create profile with 2 hidden layers of 4 neurons each, utilizing the tanh activation function
+def profile3():
+    return profile([4,4],actfunc.activation_tanh)
+
+#Create profile with 2 hidden layers of 4 neurons each, utilizing the leaky ReLU activation function
+def profile4():
+    return profile([4,4],actfunc.activation_leaky_relu)
+
+#Create profile with 2 hidden layers of 4 neurons each, utilizing the eLU activation function
+def profile5():
+    return profile([4,4],actfunc.activation_elu)
