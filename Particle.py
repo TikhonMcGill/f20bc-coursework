43f6d4863049ce43b40814298d7c5dd3c8b70461
Tikhon McGill <tm2008@hw.ac.uk>
2023-10-29 09:36:09 +0000

    Create Basic Particle Class

---
+++ Particle.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+#Particle Class which holds a vector and a velocity
+class Particle:
+    #Create a new Particle with a vector of the vector size and a vector velocity of the vector size
+    def __init__(self,vector_size):
+        self.vector = np.zeros(vector_size)
+        self.velocity = np.zeros(vector_size)
