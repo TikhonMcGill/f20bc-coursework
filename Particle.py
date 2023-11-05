777059b2a6d7e1bf4a2c9a4675318005433d7ee1
Tikhon McGill <tm2008@hw.ac.uk>
2023-11-05 11:13:15 +0000

    Add personal best attribute to Particle

--- Particle.py
+++ Particle.py
@@ -4,9 +4,15 @@ import numpy as np
 class Particle:
     #Create a Particle with a position of the vector size, and the velocity being initialized at 0
     def __init__(self,vector_size : int):
+        #Initialize the Position
         self.position = np.zeros(vector_size)
+
+        #Initialize the Velocity
         self.velocity = np.zeros(vector_size)
+
+        #Initialize the "Personal Best" - the position at which the Particle had the best performance
+        self.personal_best = np.zeros(vector_size)
     
-    #Update the Particle's Vector based on its velocity
+    #Update the Particle's Position based on its Velocity
     def update_position(self):
         self.position = np.add(self.position,self.velocity)
