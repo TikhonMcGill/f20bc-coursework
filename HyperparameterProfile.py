d2578b4cecb6a685adcb84fed53545f9210a58b1
Tikhon McGill <tm2008@hw.ac.uk>
2023-11-05 12:25:55 +0000

    Add no. particles to HyperparameterProfile

--- HyperparameterProfile.py
+++ HyperparameterProfile.py
@@ -1,12 +1,19 @@
 class HyperparameterProfile:
     def __init__(self,network_layer_sizes,network_layer_activations,
-                 pso_inertial_weight,pso_cognitive_weight,pso_social_weight,pso_global_weight,pso_iterations):
+                 pso_inertial_weight,pso_cognitive_weight,pso_social_weight,pso_global_weight,
+                 pso_iterations,pso_no_particles):
         
+        #Initialize hyperparameters of the Neural Network
         self.layer_sizes = network_layer_sizes
         self.activation_functions = network_layer_activations
         
+        #Initialize the weights
         self.a = pso_inertial_weight
         self.b = pso_cognitive_weight
         self.g = pso_social_weight
         self.gl = pso_global_weight
+        
+        #Initialize other hyperparameters of PSO
         self.iterations = pso_iterations
+        self.no_particles = pso_no_particles
+    
