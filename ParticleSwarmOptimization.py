fee893bee6ee73ccf7e6a8973371e797bb85850e
Tikhon McGill <tm2008@hw.ac.uk>
2023-11-05 11:44:56 +0000

    Create plan for PSO iteration

--- ParticleSwarmOptimization.py
+++ ParticleSwarmOptimization.py
@@ -21,38 +21,45 @@ class ParticleSwarmOptimization:
             #Fill the particles array with particles, with random velocities and positions
             #encoding the weights and biases of the neural network
             
-            #Get the size of the Particle's Vector, based on the layer sizes of the Neural Network given
-            vector_size = pc.get_particle_layer_counts(nn_layer_sizes)
+            #Get the size of the Particle's Vector, based on the layer sizes of the Neural Network given.
+            #Also, keep this vector size, since it'll be useful in future calculations (e.g. velocity etc.)
+            self.vector_size = pc.get_particle_layer_counts(nn_layer_sizes)
 
             #Create a new Particle with this vector size
-            new_particle = Particle(vector_size)
+            new_particle = Particle(self.vector_size)
 
             #Initialize the Particle's Position, with each dimension's value being between 0 and N, where N is the number
             #of particles
-            new_particle.position = np.random.rand(vector_size) * no_particles
+            new_particle.position = np.random.rand(self.vector_size) * no_particles
 
             #Initialize the Particle's Velocity, each dimension's value being between 0 and N/2, where N = no. particles
-            new_particle.velocity = np.random.rand(vector_size) * no_particles/2
+            new_particle.velocity = np.random.rand(self.vector_size) * no_particles/2
 
             #Add the New Particle to the Particles Array
             particles.append(new_particle)
 
 
-        #initialize the global best
-        best = 0
-        #For the number of iterations do:
+        #Initialize the value of the global best, and its position
+        global_best = 0
+        global_best_position = np.zeros(self.vector_size)
+        
+        #For the number of iterations, go through each particle and:
+        #1. Update its position based on current velocity
+        #2. Evaluate the Fitness of its Position
+        #3. If the Fitness at this position Exceeds the Particle's Personal best, update Particle's 
+        #Personal Best to be this position
+        #4. If the Fitness at this position Exceeds the Best of ALL particles, update the global best and
+        #the global best position to be, respectively, this particle's fitness and this particle's position
+        #5. Update the velocity of the Particle, based on inertia, the cognitive factor (its personal best),
+        #and the social factor (pick N/10 informants, where N is no. particles, calculate average of their personal bests),
+        #and the global best position
 
-            #go through each particle and acess fitness
-                #update the global best
+        for iteration in range(iterations):
+            pass
 
-            
-            #gather information for every particle
-                #for every particles velocity update each dimension
-            
-            #update each particles velocity
 
-        #return the best
-        return best
+        #Return the Global Best
+        return global_best
 
     def access_fitness(self,particle):
         pass
